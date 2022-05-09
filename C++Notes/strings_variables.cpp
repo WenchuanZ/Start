4898b40868781f47de15789ff69ca2f9ed58afa7
WenchuanZ <105089229+WenchuanZ@users.noreply.github.com>
2022-05-09 14:36:19 +1000

    Update strings_variables.cpp

--- C++Notes/strings_variables.cpp
+++ C++Notes/strings_variables.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+
+int main()
+{
+    // boy i love baconand ham
+    // boy i love bacon and ham
+    
+    
+   cout << "boy i love bacon";  
+   cout << "and ham" << endl; 
+
+   cout << "boy i love bacon ";
+   cout << "and ham" << endl;
+   
+   // You can also use '\n' to start a new line.
+   // boy i love bacon 
+    //
+   // and ham
+   cout << "boy i love bacon \n \n";
+   cout << "and ham \n";
+
+   return 0; 
+} 
